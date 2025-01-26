d6fdeb7d5d631575f9d43fe58f4b371a7f554f85
ObsureBat <ayushsharmasd03@gmail.com>
2025-01-26 16:31:55 +0530

    Update submodule

---
+++ test.js
@@ -0,0 +1,41 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector('#main'),
+        smooth: true
+    });
+
+   
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector('#main'),
+        smooth: true
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector('#main'),
+        smooth: true
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector('#main'),
+        smooth: true
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector('#main'),
+        smooth: true
+    });
+
+});
